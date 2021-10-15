1f13fe4fb2a5fd66cac108112198ff549fb44386
Cipz <matteo.cipone@gmail.com>
2021-10-15 15:43:29 +0200

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Matteo Cipone on 15.10.21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
